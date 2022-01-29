e58bf1ef784e96f309190a8cf79ace4b2789d002
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-29 10:36:47 +0100

    creating a ts client and request

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello from the app.ts file");
+import { HelloReply, HelloRequest } from "./protogen/proto/hello_pb";
+
+import { HelloWorldClient } from "./protogen/proto/hello_grpc_pb";
+
+const client = new HelloWorldClient("http://localhost:8080", null);
+const req = new HelloRequest();
+req.setName("World");
+client.sayHello(req, (err: any, response: HelloReply) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(response.getMessage());
+});
