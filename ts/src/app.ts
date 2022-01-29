bbf5ef62efdabbf080d130bda682291f781f4180
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-29 10:50:17 +0100

    adding insecure credentials from `grpc` package

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -1,14 +1,17 @@
-import { HelloReply, HelloRequest } from "./protogen/proto/hello_pb";
-
+import { HelloRequest } from "./protogen/proto/hello_pb";
 import { HelloWorldClient } from "./protogen/proto/hello_grpc_pb";
+import { credentials } from "grpc";
 
-const client = new HelloWorldClient("http://localhost:8080", null);
+const client = new HelloWorldClient(
+  "http://localhost:8080",
+  credentials.createInsecure()
+);
 const req = new HelloRequest();
 req.setName("World");
-client.sayHello(req, (err: any, response: HelloReply) => {
+client.sayHello(req, (err, response) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(response.getMessage());
+  console.log(response?.getMessage());
 });
